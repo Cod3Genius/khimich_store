ffa18658a1b3a0e0deacc803c43107812815af93
Cod3Genius <juntadirectiva@geniusindustries.org>
2024-10-03 21:44:23 +0200

    Khimich Store v0.0.7

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+/*module.exports = {
+  content: ["./src.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Bricolage Grotesque Variable",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};*/
+
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -11,6 +29,12 @@ module.exports = {
           "Inter",
           ...defaultTheme.fontFamily.sans,
         ],
+      },
+      colors: {
+        'custom-pink': '#ffb5ca', // Define un color rosa personalizado
+      },
+      backgroundImage: {
+        'gradient-pink-white': 'linear-gradient(to bottom, #ffffff, theme("colors.custom-pink"))', // Usa el color personalizado en el degradado
       },
     },
   },
